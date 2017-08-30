53e59c29a3c638d15242acba2c10e2361c984de2
Yun Li <nzdeveloper2012@gmail.com>
2017-08-30 14:50:46 +1200

    added run_main script to process data for each client code

---
+++ run_main.pl
@@ -0,0 +1,10 @@
+use strict;
+use IPC::System::Simple qw(system systemx capture capturex);
+
+my @args = ("MSD", "STHX");
+
+foreach my $arg_variable (@args) { 
+    print "processing "."[ ".$arg_variable." ]"."\n";
+	system('bar_chart.pl'.' '.'"'.$arg_variable.'"');
+	#exec('bar_chart.pl'.' '.'"'.$arg_variable.'"');
+} 
